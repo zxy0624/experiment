21646df9af8ebe0640a5ba1d9b5c12db8b856561
Xiangyu Zeng <1683609586@qq.com>
2025-03-12 16:34:02 +0800

    修改

--- CNN.py
+++ CNN.py
@@ -11,6 +11,20 @@ class MyLayer(nn.Module):
 
 layer = MyLayer()
 # 保存计算结果到变量
-result = layer(torch.tensor([1, 2, 3, 4, 5], dtype=torch.float))
+result = layer(torch.tensor([1., 2., 3., 4., 5.]))
 # 输出计算结果
+print(layer)
 print(result)
+
+class MyListDense(nn.Module):
+    def __init__(self):
+        super(MyListDense, self).__init__()
+        self.params = nn.ParameterList([nn.Parameter(torch.randn(4, 4)) for i in range(3)])
+        self.params.append(nn.Parameter(torch.randn(4, 1)))
+
+    def forward(self, x):
+        for i in range(len(self.params)):
+            x = torch.mm(x, self.params[i])
+        return x
+net = MyListDense()
+print(net)
