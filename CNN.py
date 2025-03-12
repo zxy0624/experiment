61171089178eac1327205cb701ecf2a231c53f7c
Xiangyu Zeng <1683609586@qq.com>
2025-03-12 15:49:01 +0800

    修改

--- CNN.py
+++ CNN.py
@@ -1,11 +1,16 @@
 import torch
 from torch import nn
+import numpy
 
 class MyLayer(nn.Module):
     def __init__(self, **kwargs):
         super(MyLayer, self).__init__(**kwargs)
+
     def forward(self, x):
         return x - x.mean()
 
 layer = MyLayer()
-layer(torch.tensor([1, 2, 3, 4, 5], dtype=torch.float))
+# 保存计算结果到变量
+result = layer(torch.tensor([1, 2, 3, 4, 5], dtype=torch.float))
+# 输出计算结果
+print(result)
