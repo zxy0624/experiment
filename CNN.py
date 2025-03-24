0175ade0bc55c0b218e9c1167592a439b9217ec5
Xiangyu Zeng <1683609586@qq.com>
2025-03-24 16:48:23 +0800

    feat(CNN): 实现 FashionMNIST 数据集的卷积神经网络分类模型
    - 添加了数据预处理和加载的代码，支持从 CSV 文件读取数据
    -构建了自定义的 FMDataset 类，用于处理 FashionMNIST 数据
    - 设计了一个简单的卷积神经网络模型 Net
    - 实现了模型的训练和验证函数
    - 添加了模型保存的代码

--- CNN.py
+++ CNN.py
@@ -1,30 +1,142 @@
+
+import os
+import numpy as np
+import pandas as pd
 import torch
-from torch import nn
-import numpy
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
 
-class MyLayer(nn.Module):
-    def __init__(self, **kwargs):
-        super(MyLayer, self).__init__(**kwargs)
+# 配置GPU，这里有两种方式
+## 方案一：使用os.environ
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-    def forward(self, x):
-        return x - x.mean()
+## 配置其他超参数，如batch_size, num_workers, learning rate, 以及总的epochs
+batch_size = 256
+num_workers = 0   # 对于Windows用户，这里应设置为0，否则会出现多线程错误
+lr = 1e-4
+epochs = 100
 
-layer = MyLayer()
-# 保存计算结果到变量
-result = layer(torch.tensor([1., 2., 3., 4., 5.]))
-# 输出计算结果
-print(layer)
-print(result)
+# 首先设置数据变换
+from torchvision import transforms
 
-class MyListDense(nn.Module):
+image_size = 28
+data_transform = transforms.Compose([
+    transforms.ToPILImage(),
+     # 这一步取决于后续的数据读取方式，如果使用内置数据集读取方式则不需要
+    transforms.Resize(image_size),
+    transforms.ToTensor()
+])
+## 读取方式二：读入csv格式的数据，自行构建Dataset类
+# csv数据下载链接：https://www.kaggle.com/zalando-research/fashionmnist
+class FMDataset(Dataset):
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.transform = transform
+        self.images = df.iloc[:, 1:].values.astype(np.uint8)
+        self.labels = df.iloc[:, 0].values
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx].reshape(28, 28, 1)
+        label = int(self.labels[idx])
+        if self.transform is not None:
+            image = self.transform(image)
+        else:
+            image = torch.tensor(image / 255., dtype=torch.float)
+        label = torch.tensor(label, dtype=torch.long)
+        return image, label
+
+train_df = pd.read_csv("./FashionMNIST/fashion-mnist_train.csv")
+test_df = pd.read_csv("./FashionMNIST/fashion-mnist_test.csv")
+train_data = FMDataset(train_df, data_transform)
+test_data = FMDataset(test_df, data_transform)
+
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+
+
+
+
+image, label = next(iter(train_loader))
+print(image.shape, label.shape)
+plt.imshow(image[0][0], cmap="gray")
+plt.show()
+print(train_data[0][0].shape)
+
+
+class Net(nn.Module):
     def __init__(self):
-        super(MyListDense, self).__init__()
-        self.params = nn.ParameterList([nn.Parameter(torch.randn(4, 4)) for i in range(3)])
-        self.params.append(nn.Parameter(torch.randn(4, 1)))
+        super(Net, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Dropout(0.3),
+            nn.Conv2d(32, 64, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2),
+            nn.Dropout(0.3)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(64 * 4 * 4, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
 
     def forward(self, x):
-        for i in range(len(self.params)):
-            x = torch.mm(x, self.params[i])
+        x = self.conv(x)
+        x = x.view(-1, 64 * 4 * 4)
+        x = self.fc(x)
+        # x = nn.functional.normalize(x)
         return x
-net = MyListDense()
-print(net)
+
+
+model = Net()
+model = model.cuda()
+# model = nn.DataParallel(model).cuda()   # 多卡训练时的写法，之后的课程中会进一步讲解
+criterion = nn.CrossEntropyLoss()
+# criterion = nn.CrossEntropyLoss(weight=[1,1,1,1,3,1,1,1,1,1])
+
+optimizer = optim.Adam(model.parameters(), lr = lr)
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for data, label in train_loader:
+        data, label = data.cuda(), label.cuda()
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()*data.size(0)
+    train_loss = train_loss/len(train_loader.dataset)
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+def val(epoch):
+    model.eval()
+    val_loss = 0
+    gt_labels = []
+    pred_labels = []
+    with torch.no_grad():
+        for data, label in test_loader:
+            data, label = data.cuda(), label.cuda()
+            output = model(data)
+            preds = torch.argmax(output, 1)
+            gt_labels.append(label.cpu().data.numpy())
+            pred_labels.append(preds.cpu().data.numpy())
+            loss = criterion(output, label)
+            val_loss += loss.item()*data.size(0)
+    val_loss = val_loss/len(test_loader.dataset)
+    gt_labels, pred_labels = np.concatenate(gt_labels), np.concatenate(pred_labels)
+    acc = np.sum(gt_labels==pred_labels)/len(pred_labels)
+    print('Epoch: {} \tValidation Loss: {:.6f}, Accuracy: {:6f}'.format(epoch, val_loss, acc))
+for epoch in range(1, epochs+1):
+    train(epoch)
+    val(epoch)
+
+save_path = "./FahionModel.pkl"
+torch.save(model, save_path)
